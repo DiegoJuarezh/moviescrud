b37def6794a9c49a87c5506f3d0cb84bd07f0ec1
unknown <diego.juarezh@gmail.com>
2021-04-12 03:18:12 -0500

    Generamos una Interfaz & un nuevo Servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FormComponent } from './form/form.component';
-
 import { Route,RouterModule } from '@angular/router';
-// constante de rutas
+import { HttpClientModule } from '@angular/common/http';
 const routes: Route[] = [
   {path:'',     component: HomeComponent},
   {path:'home', component: HomeComponent},
@@ -23,7 +22,8 @@ const routes: Route[] = [
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MoviesService} from '../services/movies.service';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  API_ENDPOINT = 'http://127.0.0.1:8000/api';
+  constructor(private movieService: MoviesService, private httpClient: HttpClient) {
+    httpClient.get(this.API_ENDPOINT + '/movies').subscribe( (data) => {
+      console.log(data);
+    } );
+    
+  }
 
   ngOnInit(): void {
   }
