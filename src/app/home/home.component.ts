2aaf2f8a7a45bf8dbe06ffcd21708ed0e4f601dd
unknown <diego.juarezh@gmail.com>
2021-04-12 03:36:12 -0500

    Se realizo la carga exitosa en Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MoviesService} from '../services/movies.service';
 import {HttpClient} from '@angular/common/http';
+import { Movie } from '../interfaces/movie';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +10,13 @@ import {HttpClient} from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
   API_ENDPOINT = 'http://127.0.0.1:8000/api';
+  movies: Movie[];
   constructor(private movieService: MoviesService, private httpClient: HttpClient) {
-    httpClient.get(this.API_ENDPOINT + '/movies').subscribe( (data) => {
+    this.movies = [];
+    httpClient.get(this.API_ENDPOINT + '/movies').subscribe( (data: any) => {// En lugar de 'any' era 'Movies[]'
       console.log(data);
+      this.movies = data;
     } );
-    
   }
 
   ngOnInit(): void {
