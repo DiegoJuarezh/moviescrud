006dbe6ec7ab108c25c42efa5687b0b3bb18d219
unknown <diego.juarezh@gmail.com>
2021-04-12 03:58:38 -0500

    Configurando vista form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from '../interfaces/movie';
 
 @Component({
   selector: 'app-form',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  movie: Movie = {
+    name: "",
+    description: "",
+    genre: "",
+    year: 0,
+    duration: ""
+  };
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  saveMovie(){
+    console.log(this.movie);
   }
 
 }
